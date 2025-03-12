ca16033b1a8be8a5658ae3551f647f289fa6b406
easybulb <easybulb@gmail.com>
2025-03-12 08:58:25 +0000

    Update populate_locks with correct associations

--- main/management/commands/populate_locks.py
+++ main/management/commands/populate_locks.py
@@ -1,3 +1,4 @@
+# main/management/commands/populate_locks.py
 from django.core.management.base import BaseCommand
 from main.models import TTLock, Room
 
@@ -5,8 +6,9 @@ class Command(BaseCommand):
     help = 'Populates TTLock entries and associates them with Rooms'
 
     def handle(self, *args, **options):
-        # Create TTLock entries 
+        # Create TTLock entries
         ttlock_data = [
+            {"name": "Front Door", "lock_id": 18159702},
             {"name": "Room 1", "lock_id": 21168210},
             {"name": "Room 2", "lock_id": 21168756},
             {"name": "Room 3", "lock_id": 21169056},
@@ -16,7 +18,7 @@ class Command(BaseCommand):
         for data in ttlock_data:
             TTLock.objects.get_or_create(lock_id=data["lock_id"], defaults={"name": data["name"]})
 
-        # Associate TTLock with Rooms
+        # Associate TTLock with Rooms (only for room-specific locks)
         room_locks = {
             "Room 1": 21168210,
             "Room 2": 21168756,
@@ -29,4 +31,5 @@ class Command(BaseCommand):
             ttlock = TTLock.objects.get(lock_id=lock_id)
             room.ttlock = ttlock
             room.save()
+
         self.stdout.write(self.style.SUCCESS('Successfully populated TTLock entries and associations'))
